500b0b5fc9a47bb5e9f9e9e803144f5ed9b73196
Neto test <victor.v.mankov@gmail.com>
2023-10-05 20:53:43 +0300

    new commit_2

--- logistic/views.py
+++ logistic/views.py
@@ -1,11 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
+from django_filters.rest_framework import DjangoFilterBackend, filters, FilterSet
+
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    #filter_backends = [filters.SearchFilter]
+    filter_backends = [DjangoFilterBackend]
     search_fields = ['title', 'description']
 
 
@@ -13,5 +15,11 @@ class ProductViewSet(ModelViewSet):
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['products']
 
 
+class ProductFilter(FilterSet):
+    class Meta:
+        model = Product
+        fields = ('title',)
